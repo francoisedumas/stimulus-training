50a2c56245a6cb75037249961d22a783339beddc
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-11-30 17:58:04 +0100

    setup for keydown action but doesn't work

--- app/javascript/controllers/drum_controller.js
+++ app/javascript/controllers/drum_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from 'stimulus';
 export default class extends Controller {
-  static values = { sound: String }
+  static values = { sound: String, key: Number }
 
   play() {
     const sound_path = `sounds/${this.soundValue}`
     const sound = new Audio(sound_path);
     sound.play();
+  }
+
+  playKeyboard(event) {
+    console.log("keydown")
+    console.log(event.keyCode)
+    console.log(this.keyValue)
+
+    if (event.keyCode == this.keyValue) {
+      this.play()
+    }
   }
 }
