24fdcf60cafef7ad6efc9b1a21951439e73ee4ea
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-12-03 15:58:18 +0100

    css and remove console log

--- app/javascript/controllers/form_submit_controller.js
+++ app/javascript/controllers/form_submit_controller.js
@@ -3,11 +3,7 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["day"]
 
-  connect() {
-    console.log('hello')
-  }
   handleChange() {
-    console.log('hello handle change')
     this.element.submit();
   }
 }
